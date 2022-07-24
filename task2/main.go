815d37ae624984300ebd59469e8615fffd3489d9
Vladimir814 <vladimir_814@ukr.net>
2022-07-24 16:22:58 +0300

    Add realiztion task2

--- task2/main.go
+++ task2/main.go
@@ -5,13 +5,31 @@ package main
 // [ [ 4, 6 ], [ 7, 9 ] ]
 // Результат друку:
 // “result: 26”
+
+import "fmt"
+
+
 func main() {
 	// Розкоментуй мене)
-	// n := [][]int{
-	// 	{2, 6, 9, 24},
-	// 	{7, 3, 94, 3, 0},
-	// 	{4, 2, 8, 35},
-	// }
-
+	n := [][]int{
+		{2, 6, 9, 24},
+		{7, 3, 94, 3, 0},
+		{4, 2, 8, 35},
+	}
 	// Ваша реалізація
+	var (
+		sum int
+		ch  = make(chan int)
+	)
+
+	for _, slice := range n {
+		for _, v := range slice {
+			go func(ch chan int, v int) {
+				ch <- v
+			}(ch, v)
+			sum += <-ch
+		}
+	}
+	close(ch)
+	fmt.Printf("result: %d\n", sum)
 }

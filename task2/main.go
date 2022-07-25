1ff3eb6576a8083b2a0fa92f170e7990b7f57af3
Vladimir814 <vladimir_814@ukr.net>
2022-07-25 22:06:48 +0300

    Fix task2

--- task2/main.go
+++ task2/main.go
@@ -23,12 +23,14 @@ func main() {
 	)
 
 	for _, slice := range n {
-		for _, v := range slice {
-			go func(ch chan int, v int) {
-				ch <- v
-			}(ch, v)
-			sum += <-ch
-		}
+		go func (ch chan int,slice []int)  {
+			res := 0
+		    for _, v := range slice {
+			    res += v
+			}
+			ch <- res
+		}(ch, slice)
+		sum += <-ch
 	}
 	close(ch)
 	fmt.Printf("result: %d\n", sum)
